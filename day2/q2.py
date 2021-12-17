004837d46d7544cef580c4eb8df4662e0a612e62
gh6298 <helen.harmer@ukaea.uk>
2021-12-17 17:17:10 +0000

    day 2 coding

---
+++ day2/q2.py
@@ -0,0 +1,17 @@
+input = open("input.txt", "r")
+hor_pos = 0
+dep_pos = 0
+aim = 0
+
+for line in input:
+    
+    command = line.split(" ")[0]
+    if command == "forward":
+        hor_pos += int((line.split(" ")[1]).split("\n")[0])
+        dep_pos += int((line.split(" ")[1]).split("\n")[0]) * aim
+    elif command == "down":
+        aim += int((line.split(" ")[1]).split("\n")[0])
+    else:
+        aim -= int((line.split(" ")[1]).split("\n")[0])
+
+print("depth: ", dep_pos, " - horizontal: ", hor_pos, " - muiltiplied: ", dep_pos*hor_pos)
